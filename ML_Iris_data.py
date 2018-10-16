373e430ace078043c30d7349b7972d295521dbdd
Srinikethan Mankala <sriz1392@gmail.com>
2018-10-17 00:47:10 +0530

    K-Fold cross validation, Test and train knn,logistic regression algorithms

--- ML_Iris_data.py
+++ ML_Iris_data.py
@@ -4,6 +4,7 @@ import sklearn
 from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 iris = load_iris()
 sklearn.utils.Bunch
@@ -33,3 +34,6 @@ prediction_logisticReg = logisticreg.predict([[5.1,3.5,1.4,0.2],[6.3,3.3,4.7,1.6
 print("\n the prediction using logistic reg is:")
 print(prediction_logisticReg)
 # --------------------------------------------------------------------#
+
+
+
